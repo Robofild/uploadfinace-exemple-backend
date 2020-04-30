e2ef80c247b69d2631040c337b861df9db5021fb
robofild <suporterobofild@gmail.com>
2020-04-30 20:44:06 -0300

    adicionado procfile

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,14 @@ module.exports = {
   
     client: 'postgresql',
     connection: {
-      database: 'tasks',
-      user:     'postgres',
-      password: '123456'
+      database: 'd1c23af0s7kd3h',
+      user:     'suporterobofild@gmail.com',
+      password: 'joaomiguel_21H',
+      port:5432
     },
     pool: {
       min: 2,
-      max: 10
+      max: 100
     },
     migrations: {
       tableName: 'knex_migrations'
