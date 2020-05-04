449d708b70afc4a621f1d57378cac59da3c85d7e
robofild <suporterobofild@gmail.com>
2020-05-04 15:39:54 -0300

    enviando comunicado ao banco

--- knexfile.js
+++ knexfile.js
@@ -4,16 +4,16 @@ if (process.env.NODE_ENV=="production"){
 
 module.exports = {
   
-    client: 'postgresql',
+  Host: 'ec2-34-195-169-25.compute-1.amazonaws.com',
     connection: {
-      database: 'd1c23af0s7kd3h',
-      user:     'suporterobofild@gmail.com',
-      password: 'joaomiguel_21H',
+      database: 'd7e1t77aff6kui',
+      user:     'dswfsvaskvceoz@gmail.com',
+      password: 'd59cc863db475c46706e09057a0a33fe5d38981f10660b8ef0d69aed197e227b',
       port:5432
     },
     pool: {
       min: 2,
-      max: 100
+      max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
