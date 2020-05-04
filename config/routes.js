7f785ee742c822c87485136aae72ceed58a3143f
robofild <suporterobofild@gmail.com>
2020-05-04 18:36:49 -0300

    com porta index

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ module.exports = app => {
      app.post('/signup', app.api.user.save)
      app.post('/signin', app.api.auth.signin)
      
-     app.route('/')
+     app.route('/teste')
      .get(app.api.testecomunicacao.Teste)
 
   
